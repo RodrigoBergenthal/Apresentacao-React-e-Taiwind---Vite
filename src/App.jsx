70d5a76b307ee5859025bf41a3539a8e9ce7f105
Rodrigo Bergenthal <rodrigo_bergenthak@live.com>
2024-10-17 21:38:21 -0300

    Portifolio em React + Tailwind + Vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Meu Portifólio</h1>
+      <p>Aqui está um pouco sobre mim e alguns dos meus projetos.</p>
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div>Rodrigo Bergenthal</div>
+            <div>
+                <a href="#home">Início</a>
+                <a href="#home">Sobre Min</a>
+                <a href="#home">Serviços</a>
+                <a href="#home">Projetos</a>
+                <a href="#home">Contato</a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
